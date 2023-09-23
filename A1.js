2964a13f9632d394d2b3033545edd40d837b9587
Sai Kumar Gottapu <gottapu@sheridancollege.ca>
2023-09-23 18:33:51 -0400

    Part 3 Done

--- A1.js
+++ A1.js
@@ -1,38 +1,91 @@
 //Part 1
 const hospital = {
-    name: "Oakville General Hospital",
+  name: "Oakville General Hospital",
   patients: [
-        {
-            id: 1,
-  name: "Joey Tribbiani",
-  dateOfBirth: "July 25th, 1967",
-  symptoms: ["headache", "back pain", "fever"]
-        },
-        {
-            id: 2,
-  name: "Chandler Bing",
-  dateOfBirth: "August 19th, 1969",
-  symptoms: ["memory loss", "cold", "shivering"]
-        },
-        {
-            id: 3,
-  name: "David Schwimmer",
-  dateOfBirth: "November 2nd, 1966",
-  symptoms: ["dizzyness", "shortness of breath", "headache"]
-        }
-    ]
-}
+    {
+      id: 1,
+      name: "Joey Tribbiani",
+      dateOfBirth: "July 25th, 1967",
+      symptoms: ["headache", "back pain", "fever"],
+    },
+    {
+      id: 2,
+      name: "Chandler Bing",
+      dateOfBirth: "August 19th, 1969",
+      symptoms: ["memory loss", "cold", "shivering"],
+    },
+    {
+      id: 3,
+      name: "Ross Gellar",
+      dateOfBirth: "November 2nd, 1966",
+      symptoms: ["dizzyness", "shortness of breath", "headache"],
+    },
+  ],
+};
 //End of Part 1
 
 //Part 2
 
 function showPatients(hospitalData) {
+  let finalOutput = "<h1>" + hospitalData.name + "</h1>";
 
-    const hospitalName = hospitalData.name;
-    const patients = hospitalData.patient;
+  for (let i = 0; i < 3; i++) {
+    finalOutput =
+      finalOutput +
+      "<h2>" +
+      hospitalData.patients[i].name +
+      ", " +
+      hospitalData.patients[i].dateOfBirth +
+      "</h2> \n";
+    finalOutput = finalOutput + "<ul> \n";
 
+    let symptoms = "";
+    for (let j = 0; j < 3; j++) {
+      symptoms =
+        symptoms + "<li>" + hospitalData.patients[i].symptoms[j] + "</li>\n";
+    }
+
+    finalOutput = finalOutput + symptoms;
+    finalOutput = finalOutput + "</ul> \n";
+  }
+
+  //   finalOutput = finalOutput + "<h2>" + hospitalData.patients[0].name + ", " + hospitalData.patients[0].dateOfBirth  + "</h2> \n";
+  //     finalOutput = finalOutput + "<h2>" + hospitalData.patients[1].name + ", " + hospitalData.patients[1].dateOfBirth  + "</h2> \n";
+  //     finalOutput = finalOutput + "<h2>" + hospitalData.patients[2].name + ", " + hospitalData.patients[2].dateOfBirth  + "</h2> \n";
+  return finalOutput;
 }
 
-console.log("<h1>Oakville General Hospital</h1>");
+console.log(showPatients(hospital));
+
+// End of Part 2
 
 // patients.
+
+// <h1>Oakville</h1>
+
+//<h2>Joey Tribbiani, July 25th, 1967 </h2>
+//<ul>
+//<li>headache</li>
+//<li>back pain</li>
+//<li>fever</li>
+//</ul>
+
+//<h2>Joey Tribbiani, July 25th, 1967 </h2>
+//<ul>
+//<li>headache</li>
+//<li>back pain</li>
+//<li>fever</li>
+//</ul>
+
+//<h2>Joey Tribbiani, July 25th, 1967 </h2>
+//<ul>
+//<li>headache</li>
+//<li>back pain</li>
+//<li>fever</li>
+//</ul>
+
+// Part 3
+function getPatient(patients) {
+  return patients[Math.floor(Math.random() * patients.length)].id;
+}
+console.log(getPatient(hospital.patients))
