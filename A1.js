3273d7398f975050ffbdc2ef4f3000ed385366fd
Sai Kumar Gottapu <gottapu@sheridancollege.ca>
2023-09-23 15:02:38 -0400

    Initial commit

---
+++ A1.js
@@ -0,0 +1,38 @@
+//Part 1
+const hospital = {
+    name: "Oakville General Hospital",
+  patients: [
+        {
+            id: 1,
+  name: "Joey Tribbiani",
+  dateOfBirth: "July 25th, 1967",
+  symptoms: ["headache", "back pain", "fever"]
+        },
+        {
+            id: 2,
+  name: "Chandler Bing",
+  dateOfBirth: "August 19th, 1969",
+  symptoms: ["memory loss", "cold", "shivering"]
+        },
+        {
+            id: 3,
+  name: "David Schwimmer",
+  dateOfBirth: "November 2nd, 1966",
+  symptoms: ["dizzyness", "shortness of breath", "headache"]
+        }
+    ]
+}
+//End of Part 1
+
+//Part 2
+
+function showPatients(hospitalData) {
+
+    const hospitalName = hospitalData.name;
+    const patients = hospitalData.patient;
+
+}
+
+console.log("<h1>Oakville General Hospital</h1>");
+
+// patients.
